bff4b4d566aa67b5c5c5c642a242a87036136e63
c <67209229+ceat-ceat@users.noreply.github.com>
2021-10-09 19:41:20 -0400

    Update loader.lua

--- BBF/loader.lua
+++ BBF/loader.lua
@@ -1,7 +1,6 @@
-local defplugins = {
-    "https://raw.githubusercontent.com/ceat-ceat/BecomeFumoStuff/main/BBF/DefaultPlugins/BetterNameTags.lua"
-  }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ceat-ceat/BecomeFumoStuff/main/BBF/BBF.lua"))()
-for i, v in next, defplugins do
+for i, v in next, {
+    "https://raw.githubusercontent.com/ceat-ceat/BecomeFumoStuff/main/BBF/DefaultPlugins/BetterNameTags.lua"
+  } do
   loadstring(game:HttpGet(v))()
 end
