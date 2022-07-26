f57efada24e12b7d1eb37e79e935ae88a35cea88
c <67209229+ceat-ceat@users.noreply.github.com>
2022-07-26 15:01:56 -0400

    change httpget to not cache

--- BBF/loader.lua
+++ BBF/loader.lua
@@ -1,6 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ceat-ceat/BecomeFumoStuff/main/BBF/BBF.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ceat-ceat/BecomeFumoStuff/main/BBF/BBF.lua"), true)()
 for i, v in next, {
     "https://raw.githubusercontent.com/ceat-ceat/BecomeFumoStuff/main/BBF/DefaultPlugins/BetterNameTags.lua"
   } do
-  loadstring(game:HttpGet(v))()
+  loadstring(game:HttpGet(v, true))()
 end
